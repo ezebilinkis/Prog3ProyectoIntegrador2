2d89ff509f9bb70f33e0ee8d2d5c9a1e5be4b3fd
Trombola <trombola@udesa.edu.ar>
2023-11-14 13:35:25 -0300

    pequeno cambio

--- my-app/src/screens/ProfileOtros.js
+++ my-app/src/screens/ProfileOtros.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, FlatList, ScrollView } from 'react-native'
 import { auth, db } from '../firebase/config'
-
+import Posteo from '../components/Posteo'
 export default class Profile extends Component {
   constructor(props){
     super(props)
     this.state = {
-      usuarios:[]
+      usuarios:[],
+      posteo: [],
+      esUserLogueado: false
     }
   }
 
   componentDidMount(){
+    if(auth.currentUser.email == this.props.route.params.email){
+      this.setState({
+        esUserLogueado: true
+      })
+    }
     db.collection('users').where('owner','==', this.props.route.params.email).onSnapshot((docs)=>{
       let perfil = []
       docs.forEach((doc) => {
@@ -19,10 +26,23 @@ export default class Profile extends Component {
           data: doc.data()
         })
       })
-
+      
       this.setState({
         usuarios : perfil
       })
+    })
+    db.collection('posts').where('owner','==', this.props.route.params.email).onSnapshot((docs)=>{
+      let post = []
+      docs.forEach((doc) => {
+        post.push({
+          id:doc.id,
+          data: doc.data()
+        })
+      })
+      
+      this.setState({
+        posteo : post
+      }, ()=> console.log(this.state.posteo))
     })
   }
 
@@ -44,6 +64,28 @@ export default class Profile extends Component {
               </View>
                }
         />
+        {
+          this.state.esUserLogueado?
+          <View>
+          <TouchableOpacity
+          style={styles.signoutBtn}
+          onPress={()=> this.logout()}
+          >
+            <Text style={styles.signoutBtnText}>Cerrar sesión</Text>
+          </TouchableOpacity>
+        </View>
+          :
+          <></>
+        }
+        {/* Capaz falta un condicional en esto que podremos implementar cuando hagamos el buscador de usuarios */}
+        <ScrollView>
+          <FlatList
+            data={this.state.posteo}
+            keyExtractor={(item)=> item.id.toString()}
+            renderItem={({ item })=> <Posteo navigation={this.props.navigation} data={item.data} id={item.id}/>}
+          />
+        </ScrollView>
+        
       </View>
     )
   }
