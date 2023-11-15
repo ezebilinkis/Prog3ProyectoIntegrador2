6f66ce8d8330828bb668d820cea50c5a5b43f54a
ezebilinkis <ebilinkis@udesa.edu.ar>
2023-11-15 09:26:13 -0300

    electiva confirmacion para borrar posteos

--- my-app/src/screens/Profile.js
+++ my-app/src/screens/Profile.js
@@ -1,152 +1,212 @@
-import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet, FlatList, ScrollView, Image } from 'react-native'
-import { auth, db } from '../firebase/config'
-import Posteo from '../components/Posteo'
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, FlatList, ScrollView, Image, Alert, Modal } from 'react-native';
+import { auth, db } from '../firebase/config';
+import Posteo from '../components/Posteo';
+
 export default class Profile extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      usuarios:[],
-      posteo: [],
-      esUserLogueado: false
-    }
-  }
+    usuarios: [],
+    posteo: [],
+    esUserLogueado: false,
+    modalVisible: false,
+    postToDelete: null,
+  };
+}
 
-  componentDidMount(){
-    
-    db.collection('users').where('owner','==', auth.currentUser.email).onSnapshot((docs)=>{
-      let perfil = []
-      docs.forEach((doc) => {
-        perfil.push({
-          id:doc.id,
-          data: doc.data()
-        })
-      })
-      
-      this.setState({
-        usuarios : perfil
-      }, ()=>console.log(this.state.usuarios))
-    })
-    db.collection('posts').where('owner','==', auth.currentUser.email).onSnapshot((docs)=>{
-      let post = []
-      docs.forEach((doc) => {
-        post.push({
-          id:doc.id,
-          data: doc.data()
-        })
-      })
-      
-      this.setState({
-        posteo : post
-      }, ()=> console.log(this.state.posteo))
-    })
-    
-  }
+componentDidMount() {
+  db.collection('users').where('owner', '==', auth.currentUser.email).onSnapshot((docs) => {
+    let perfil = [];
+    docs.forEach((doc) => {
+      perfil.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
 
-  logout(){
-    auth.signOut()
-    this.props.navigation.navigate('Login')
-  }
+    this.setState({
+      usuarios: perfil,
+    }, () => console.log(this.state.usuarios));
+  });
 
-  borrarPosteo(item){
-    db.collection('posts').doc(item.id).delete()
-    .then(()=>alert('El posteo fue eliminado correctamente'))
-    
-  }
+  db.collection('posts').where('owner', '==', auth.currentUser.email).onSnapshot((docs) => {
+    let post = [];
+    docs.forEach((doc) => {
+      post.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
 
+    this.setState({
+      posteo: post,
+    }, () => console.log(this.state.posteo));
+  });
+}
 
-  render() {
-    return (
+logout() {
+  auth.signOut();
+  this.props.navigation.navigate('Login');
+}
+
+borrarPosteo(item) {
+  db.collection('posts').doc(item.id).delete()
+  .then(() => alert('El posteo fue eliminado correctamente'));
+}
+
+confirmarBorrarPosteo(item) {
+  this.setState({ modalVisible: true, postToDelete: item });
+}
+
+realizarBorrarPosteo() {
+  this.borrarPosteo(this.state.postToDelete);
+  this.setState({ modalVisible: false, postToDelete: null });
+}
+
+cancelarBorrarPosteo() {
+  this.setState({ modalVisible: false, postToDelete: null });
+}
+
+render() {
+  return (
+    <ScrollView>
+      <Text style={styles.miPerfilTxt}>Perfil</Text>
+      <FlatList
+        data={this.state.usuarios}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.container}>
+            <Text style={styles.datosMiPerfilTxt}>Nombre de usuario: {item.data.name}</Text>
+            <Text style={styles.datosMiPerfilTxt}>Minibio: {item.data.minibio}</Text>
+            <Text style={styles.datosMiPerfilTxt}>Email: {item.data.owner}</Text>
+            {item.data.fotoPerfil == '' ? (
+              <></>
+            ) : (
+              <Image source={{ uri: item.data.fotoPerfil }} style={styles.img} />
+            )}
+            <Text style={styles.datosMiPerfilTxt}>Cantidad de posteos: {this.state.posteo.length}</Text>
+          </View>
+        )}
+      />
+      <View>
+        <TouchableOpacity style={styles.signoutBtn} onPress={() => this.logout()}>
+          <Text style={styles.signoutBtnText}>Cerrar sesión</Text>
+        </TouchableOpacity>
+      </View>
       <ScrollView>
-        
-        <Text style={styles.miPerfilTxt}>Perfil</Text>
-        {console.log(this.state.usuarios)}
-          <FlatList
-            data={this.state.usuarios}
-            keyExtractor={(item)=> item.id.toString() }
-            renderItem={ ( {item} ) => <View style={styles.container}>
-                <Text style={styles.datosMiPerfilTxt}>Nombre de usuario: {item.data.name}</Text>
-                <Text style={styles.datosMiPerfilTxt}>Minibio: {item.data.minibio}</Text>
-                <Text style={styles.datosMiPerfilTxt}>Email: {item.data.owner}</Text>
-                {
-                  item.data.fotoPerfil == ''?
-                  <></>
-                  :
-                  <Image source={{uri: item.data.fotoPerfil}} style = {styles.img}/>
-                }
-                <Text style={styles.datosMiPerfilTxt}>Cantidad de posteos: {this.state.posteo.length}</Text>
-              </View>
-               }
+        <FlatList
+          data={this.state.posteo}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View>
+              <Posteo navigation={this.props.navigation} data={item.data} id={item.id} />
+              <TouchableOpacity onPress={() => this.confirmarBorrarPosteo(item)}>
+                <Text>Borrar Posteo</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         />
-        
-          <View>
-          <TouchableOpacity
-          style={styles.signoutBtn}
-          onPress={()=> this.logout()}
-          >
-            <Text style={styles.signoutBtnText}>Cerrar sesión</Text>
-          </TouchableOpacity>
-        </View>
-        
-        <ScrollView >
-          <FlatList
-            data={this.state.posteo}
-            keyExtractor={(item)=> item.id.toString()}
-            renderItem={({ item })=> <View>
-              <Posteo navigation={this.props.navigation} data={item.data} id={item.id}/>
-                <TouchableOpacity onPress={()=> this.borrarPosteo(item)}>
-                  <Text>Borrar Posteo</Text>
-                </TouchableOpacity>
-              </View>
-              }
-            
-          />
-        </ScrollView>
-        
       </ScrollView>
-    )
-  }
+      <Modal 
+      animationType="slide" 
+      transparent={true}  
+      visible={this.state.modalVisible}>
+          <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              ¿Estás seguro de que quieres eliminar este posteo?
+            </Text>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => this.realizarBorrarPosteo()}
+            >
+              <Text style={styles.textStyle}>Aceptar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => this.cancelarBorrarPosteo()}
+            >
+              <Text style={styles.textStyle}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </ScrollView>
+  );
+}
 }
 
 const styles = StyleSheet.create({
-  signoutBtn:{ 
-  backgroundColor: 'blue',
-  padding: 10,
-  borderWidth: 1,   
-  alignItems: 'center',   
-  justifyContent: 'center', 
-  width: 150,  
-  alignSelf: 'center'
-},
-signoutBtnText: {
-  color: 'white',  
-  fontSize: 16,   
-  fontWeight: 'bold',  
-},
-miPerfilTxt:{
-  color: 'black',
-  fontSize: 25,
-  fontWeight: 'bold',  
-  alignItems: 'center',   
-  justifyContent: 'center',
-  alignSelf: 'center'
-
-
-},
-datosMiPerfilTxt:{
-  fontSize:18,
-  alignItems: 'center',   
-  justifyContent: 'center',
-  alignSelf: 'center'
-
-},
-img:{
-  height: 300,
-  width: 300
-},
-container:{
-  alignSelf: 'center',
-  flex: 1
-}
-
-})
+  signoutBtn: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    alignSelf: 'center',
+  },
+  signoutBtnText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  miPerfilTxt: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  datosMiPerfilTxt: {
+    fontSize: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  img: {
+    height: 300,
+    width: 300,
+  },
+  container: {
+    alignSelf: 'center',
+    flex: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
